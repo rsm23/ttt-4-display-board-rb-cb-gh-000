20f46073285193355029fd80d40e9fe0d058b700
Rahmani Saif Moulouk <cool2309@gmail.com>
2017-11-26 01:04:36 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,11 +1,11 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(board)
-  print " #{board[1]} | #{board[2]} | #{board[3]} \n";
+  print " #{board[0]} | #{board[1]} | #{board[2]} \n";
   puts "-----------";
 
-  print " #{board[4]} | #{board[5]} | #{board[6]} \n";
+  print " #{board[3]} | #{board[4]} | #{board[5]} \n";
   puts "-----------";
 
-  print " #{board[7]} | #{board[8]} | #{board[9]} \n";
+  print " #{board[6]} | #{board[7]} | #{board[8]} \n";
 end
